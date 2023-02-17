bc72a8a60c7dcac81c6775db1a53fb27129ee287
Abhijeet Ingle <abhijeetingle.p@gmail.com>
2023-02-17 16:50:21 +0530

    added another validation helper

---
+++ app/helper.py
@@ -0,0 +1,67 @@
+from .data import charToFuncMap
+
+def mapToFunc(arr):
+    def cb(char):
+        if char not in charToFuncMap.keys():
+            return [char]
+        else:
+            return [*charToFuncMap[char]]
+
+    ans = []
+    for char in arr:
+        ans = ans + cb(char)
+
+    return ans
+
+def validate(arr, operators):
+    # validate brackets
+    def validate_brackets(arr):
+        stack = []
+        for char in arr:
+            if char == "(":
+                stack.append(char)
+            elif char == ")":
+                if len(stack):
+                    stack.pop()
+                else:
+                    return False
+        if len(stack):
+            return False
+        else:
+            return True
+
+    # validate or other words
+    def validate_for_other_words(arr, operators):
+        for char in arr:
+            if char not in operators and mapToFunc(char) not in operators and not char.isnumeric():
+                return False
+        return True
+
+
+    # validate operator syntax
+    def validate_operator_syntax(arr, operators):
+        for idx, char in enumerate(arr):
+            if char in operators:
+                if idx == 0 or idx == len(arr) - 1 or (not (arr[idx-1].isnumeric() or arr[idx-1] in ("(", ")")) and (arr[idx+1].isnumeric() or arr[idx+1] in ("(", ")"))):
+                    return False
+        return True
+
+
+    return validate_brackets(arr) and validate_for_other_words(arr, operators) and validate_operator_syntax(arr, operators)
+
+# arr = ['12', '/', '1']
+# arr2 = ['12', 'mod', '55']
+# arr3 = ['12', 'square(x)', '5']
+# print(mapToFunc(arr))
+# print(mapToFunc(arr2))
+# print(mapToFunc(arr3))
+# arr = ["6", "+", "2", "^", "2"]
+#  oprs = [
+#             "+",
+#             "/",
+#             "*",
+#             "-",
+#             "**",
+#             "^",
+#         ]
+# print(validate(arr, oprs))
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from .data import nameMapping
+from .helper import mapToFunc, validate
 
 bp_root = Blueprint("root", __name__, url_prefix="/")
 bp_app = Blueprint("app", __name__, url_prefix="/app")
@@ -12,16 +13,40 @@ def root():
 @bp_app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST" :
-        print(request.get_json())
+        data = request.get_json()
+        infix_array = data['payload']
+
+        print(infix_array)
+
+        # map to func names
+        infix_array_updated = mapToFunc(infix_array)
+
+        # operators
+        oprs = [
+            "+",
+            "/",
+            "*",
+            "-",
+            "**",
+            "^",
+        ]
 
         # validate the expression
-
+        value = None
+        if validate(infix_array_updated, oprs):
+            try:
+                print(infix_array_updated)
+                value = eval(''.join(infix_array_updated))
+            except:
+                value = "Wrong expression"
             # if validation successfull evaluate the expression
             # and load page
 
             # else return load error page
-
+        else:
+            value = "Wrong expression"
         return {
-            "value": 'done!'
+            "value": value
         }
+
     return render_template("calculatorSkeleton.html", nameMapping=nameMapping)
